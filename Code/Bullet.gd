217c7b2a5a1eb339700cfb53e260575fc7117961
RaddiZeCat <maxraddatz98@gmail.com>
2024-10-13 18:27:24 +0200

    Next fix Pathing

--- Code/Bullet.gd
+++ Code/Bullet.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 @export var speed = 1000
 @export var damage = 1
+@export var selfkill:bool = false
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
-
+	if selfkill == true:
+		await get_tree().create_timer(0.2).timeout
+		self.queue_free()
 
 
 func _on_body_entered(body):
--- Code/Mech.gd
+++ Code/Mech.gd
@@ -9,9 +9,19 @@ extends CharacterBody2D
 @onready var muzzle2 = $Body/Gun2/Marker2D2
 @export var speed:float = 30
 @export var speed_multiplyer = 1
+@onready var cameraMarker = $CameraMarker2D
+@export var cameraSpeed = 50
 
 @export var bullet:PackedScene
+@export var minibullet:PackedScene
+@export var shotbullet:PackedScene
 @export var rocket:PackedScene
+@onready var shotL:PackedScene = bullet
+@onready var shotR:PackedScene = rocket
+
+enum GunState{SHIELD,LMG,MINIGUN,SHOTGUN,LAUNCHER}
+var gunStateL = GunState.LMG
+var gunStateR = GunState.LMG
 
 func _physics_process(delta):
 	var input_horizontal = Input.get_axis("move_left","move_right")
@@ -21,22 +31,52 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	legs.look_at(mech.position + input)
-	
 	body.look_at(get_global_mouse_position())
+	gun1.look_at(get_global_mouse_position())
+	gun2.look_at(get_global_mouse_position())
 	
+	cameraMarker.position = lerp(cameraMarker.position,(get_local_mouse_position() - body.position)*0.2,cameraSpeed*delta)
 
 func _input(event):
 	if Input.is_action_just_pressed("shoot_1"):
 		shoot1()
 	if Input.is_action_just_pressed("shoot_2"):
 		shoot2()
-
+	
 func shoot1():
-	var b = bullet.instantiate()
-	owner.add_child(b)
-	b.transform = muzzle1.global_transform
+	var l = shotL.instantiate()
+	owner.add_child(l)
+	l.transform = muzzle1.global_transform
 
 func shoot2():
-	var r = rocket.instantiate()
+	var r = shotR.instantiate()
 	owner.add_child(r)
 	r.transform = muzzle2.global_transform
+
+func setWeapon1():
+	match gunStateL:
+		GunState.SHIELD:
+			pass
+		GunState.LMG:
+			shotL = bullet
+		GunState.MINIGUN:
+			shotL = minibullet
+		GunState.SHOTGUN:
+			shotL = shotbullet
+		GunState.LAUNCHER:
+			shotL = rocket
+	pass
+
+func setWeapon2():
+	match gunStateR:
+		GunState.SHIELD:
+			pass
+		GunState.LMG:
+			shotR = bullet
+		GunState.MINIGUN:
+			shotR = minibullet
+		GunState.SHOTGUN:
+			shotR = shotbullet
+		GunState.LAUNCHER:
+			shotR = rocket
+	pass
